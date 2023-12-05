a9430bb98f3e9e621eac5bd8addab31aa66e92e9
Ian Chege <en20101832020@student.mut.ac.ke>
2023-12-05 09:53:57 +0300

    fix: update site metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,17 +6,15 @@ const siteMetadata = {
   description: 'Tech by Castynet Studios',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
-  siteLogo: '/static/images/logo.png',
+  siteUrl: 'https://castynet-blog.vercel.app/',
+  siteRepo: 'https://github.com/Ian-Chege/Castynet-Blog',
+  siteLogo: '/static/images/castynetLogo.png',
   socialBanner: '/static/images/twitter-card.png',
-  mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
+  email: 'riunge@castynet.africa',
+  github: 'https://github.com/castynet-studios',
   twitter: 'https://twitter.com/Twitter',
   facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  linkedin: 'https://www.linkedin.com/company/castynet/',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
